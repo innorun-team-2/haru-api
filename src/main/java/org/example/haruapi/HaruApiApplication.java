b717deb998352edfaea2a78d69945989a0344f80
heeheepark <pjh9641@gmail.com>
2026-08-19 06:47:22 +0200

    chore: 프로젝트 초기 셋팅

---
+++ src/main/java/org/example/haruapi/HaruApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.haruapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/haruapi/HaruApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.haruapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
