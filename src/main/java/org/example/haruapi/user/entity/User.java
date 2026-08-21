46e44f81843dd4195aa0fce22329a60edc3ba519
금선제 <goldsunje@gmail.com>
2026-08-21 10:45:59 +0900

    chore : user Entity 생성

--- src/main/java/org/example/haruapi/user/entity/User.java
+++ src/main/java/org/example/haruapi/user/entity/User.java
@@ -1,4 +1,50 @@
 package org.example.haruapi.user.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.haruapi.global.entity.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+
+
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 255)
+    private String nickname;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    private User(String email, String password, String nickname, LocalDateTime deletedAt) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.deletedAt = deletedAt;
+
+    }
+
+
+
+
+
+
+
 }
