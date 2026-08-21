9bd19ef841873b45135f1b7e2e4fdde250a513a0
heeheepark <pjh9641@gmail.com>
2026-08-21 22:30:12 +0200

    feat: 공통 Response Body 생성

---
+++ src/main/java/org/example/haruapi/global/dto/ErrorResponse.java
@@ -0,0 +1,15 @@
+package org.example.haruapi.global.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
