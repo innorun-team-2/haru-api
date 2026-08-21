0b01e2ef08d508ddfbaadb9922b367bec4f9bb69
Hseok-2 <dookie289@gmail.com>
2026-08-21 10:47:40 +0900

    feat: Post 엔티티 추가

---
+++ src/main/java/org/example/haruapi/post/entity/Post.java
@@ -0,0 +1,33 @@
+package org.example.haruapi.post.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.haruapi.global.entity.BaseEntity;
+import org.example.haruapi.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "posts")
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime deletedAt;
+}
