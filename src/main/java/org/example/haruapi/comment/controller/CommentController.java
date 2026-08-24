970a22c14e3e8d4273c06a0d4ce59f33c57446f9
heeheepark <pjh9641@gmail.com>
2026-08-24 11:18:53 +0200

    feat: RequestMapping 추가

--- src/main/java/org/example/haruapi/comment/controller/CommentController.java
+++ src/main/java/org/example/haruapi/comment/controller/CommentController.java
@@ -8,18 +8,16 @@ import org.example.haruapi.global.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/posts/{postId}/comments")
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/api/posts/{postId}/comments")
+    @PostMapping
     public ResponseEntity<ApiResponse<CommentCreateResponse>> save(
             @AuthenticationPrincipal Jwt jwt,
             @PathVariable Long postId,
