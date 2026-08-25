0efd68508b5afda8cb725d307d7d43c360176846
heeheepark <pjh9641@gmail.com>
2026-08-25 02:38:00 +0200

    feat: 테스트 코드 수정

--- src/test/java/org/example/haruapi/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/haruapi/comment/service/CommentServiceTest.java
@@ -184,24 +184,35 @@ class CommentServiceTest {
     @Test
     void 댓글_삭제_성공() {
         // given
-        Jwt jwt = Jwt.withTokenValue("test-token")
-                .header("alg", "none")
-                .claim("sub", "1")
-                .build();
+        Long userId = 1L;
+        Long postId = 1L;
+        Long commentId = 1L;
 
         User user = mock(User.class);
-        given(user.getId()).willReturn(1L);
-
+        Post post = mock(Post.class);
         Comment comment = mock(Comment.class);
-        given(comment.getUser()).willReturn(user);
 
-        given(commentRepository.findById(1L))
+        given(user.getId()).willReturn(userId);
+
+        given(postRepository.findById(postId))
+                .willReturn(Optional.of(post));
+
+        given(commentRepository.findById(commentId))
                 .willReturn(Optional.of(comment));
 
+        given(comment.getUser())
+                .willReturn(user);
+
         // when
-        commentService.delete(jwt, 1L, 1L);
+        commentService.delete(
+                userId,
+                postId,
+                commentId
+        );
 
         // then
+        verify(postRepository).findById(postId);
+        verify(commentRepository).findById(commentId);
         verify(comment).updateDeletedAt();
     }
 }
