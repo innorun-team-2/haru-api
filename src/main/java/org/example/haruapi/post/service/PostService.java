34174ce0fd47383aee241c6da395c47bd0c402a2
Hseok-2 <dookie289@gmail.com>
2026-08-25 12:06:49 +0900

    feat: 게시글 전체 조회 댓글 개수, 페이징 처리 추가, N+1 문제 해결

--- src/main/java/org/example/haruapi/post/service/PostService.java
+++ src/main/java/org/example/haruapi/post/service/PostService.java
@@ -1,6 +1,7 @@
 package org.example.haruapi.post.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.haruapi.comment.repository.CommentRepository;
 import org.example.haruapi.global.s3.service.S3Service;
 import org.example.haruapi.image.entity.Image;
 import org.example.haruapi.image.repository.ImageRepository;
@@ -12,13 +13,18 @@ import org.example.haruapi.post.exception.PostNotFoundException;
 import org.example.haruapi.post.repository.PostRepository;
 import org.example.haruapi.user.entity.User;
 import org.example.haruapi.user.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +35,7 @@ public class PostService {
     private final ImageRepository imageRepository;
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
 
     // 게시글 생성
     public PostCreateResponseDto create(Long userId, PostCreateRequestDto requestDto, MultipartFile image) {
@@ -64,15 +71,44 @@ public class PostService {
 
     // 게시글 전체 조회
     @Transactional(readOnly = true)
-    public List<PostGetAllResponseDto> getAll() {
-        // 코멘트 개수 조회 로직 필요 (나중에)
+    public Page<PostGetAllResponseDto> getAll(Pageable pageable) {
+        Page<Post> postPage = postRepository.findAllByDeletedAtIsNull(pageable); // 요청한 페이지만큼 게시글 가져옴 (기본 10개)
+        List<Post> posts = postPage.getContent(); // 게시글 추출
 
-        List<Post> posts = postRepository.findAllByDeletedAtIsNull();
-        return posts.stream()
+        // 게시글이 없다면 빈 페이지 객체 반환
+        if (posts.isEmpty()) {
+            return Page.empty(pageable);
+        }
+
+        // 조회된 10개의 게시글에서 postId만 뽑아옴
+        List<Long> postIds = posts.stream()
+                .map(Post::getId)
+                .toList();
+
+        // 10개의 이미지를 한 번에 조회, map으로 변환
+        List<Image> images = imageRepository.findByPostIdIn(postIds);
+        Map<Long, Image> imageMap = images.stream()
+                .collect(Collectors.toMap(image -> image.getPost().getId(), image -> image));
+
+        // 10개의 댓글 통계(group by)를 한 번에 조회, map으로 변환
+        List<Object[]> commentCounts = commentRepository.countByPostIdIn(postIds);
+        Map<Long, Long> commentCountMap = commentCounts.stream()
+                .collect(Collectors.toMap(
+                        result -> (Long) result[0],
+                        result -> (Long) result[1]
+                ));
+
+        // dto로 조립
+        List<PostGetAllResponseDto> dtoList = posts.stream()
                 .map(post -> {
-                    Image image = imageRepository.findByPost(post);
-                    return PostGetAllResponseDto.from(post, image);
+                    Long postId = post.getId();
+                    Image image = imageMap.get(postId);
+                    Long count = commentCountMap.getOrDefault(postId, 0L);
+
+                    return PostGetAllResponseDto.from(post, image, count);
                 }).toList();
+
+        return new PageImpl<>(dtoList, pageable, postPage.getTotalElements());
     }
 
     // 게시글 상세 조회
